cd0048f96fc4272faf40dee85dca23cf6f2a15b1
devops <devops@localhost>
2026-09-12 08:55:38 -0400

    Sync regenerated apibuilder/DAO code
    Generated by platform 0.21.96.

--- src/generated/com-bryzek-apibuilder-generator.ts
+++ src/generated/com-bryzek-apibuilder-generator.ts
@@ -204,6 +204,10 @@ export class ApiClient {
       throw new ValidationErrorsResponse(response);
     }
 
+    if (response.status === 422) {
+      throw new ValidationErrorsResponse(response);
+    }
+
     throw new ApiException(response, `Request failed with status ${response.status}`);
 
   }
